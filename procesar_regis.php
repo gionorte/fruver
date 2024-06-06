173f668422626031c4da0d68d0dc278643b46221
erik pabon <tovarpabon2301@gmail.com>
2024-06-06 17:36:56 -0500

    cambios en registrar persona

--- procesar_regis.php
+++ procesar_regis.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['Id_Cargo'])) {
+    header("Location: iniciosesion.php");
+    exit();
+}
+
 include("conexion.php");
 
 // Recibir datos del formulario
@@ -32,18 +38,28 @@ if ($row_verif_num_doc['num_rows'] > 0) {
     $telefono = $_POST['telefono'];
     $contrasena = $_POST['contrasena'];
     $id_cargo = $_POST['id_cargo'];
+    $salario = $_POST['salario'];
 
-    // Consulta SQL para insertar los datos
-    $sql_insert = "INSERT INTO persona (Num_Doc, Tipo_Doc, Prim_Nombre, Seg_Nombre, Prim_Apellido, Seg_Apellido, Genero, Telefono, Correo, Contrasena, Id_Cargo) 
-            VALUES ('$num_doc', '$tipo_doc', '$prim_nombre', '$seg_nombre', '$prim_apellido', '$seg_apellido', '$genero', '$telefono', '$correo', '$contrasena', '$id_cargo')";
+    // Consulta SQL para insertar los datos en la tabla persona
+    $sql_insert_persona = "INSERT INTO persona (Num_Doc, Tipo_Doc, Prim_Nombre, Seg_Nombre, Prim_Apellido, Seg_Apellido, Genero, Telefono, Correo, Contrasena) 
+                   VALUES ('$num_doc', '$tipo_doc', '$prim_nombre', '$seg_nombre', '$prim_apellido', '$seg_apellido', '$genero', '$telefono', '$correo', '$contrasena')";
 
-    if ($conn->query($sql_insert) === TRUE) {
-        // Establecer variable de sesión para indicar registro exitoso
-        session_start();
-        $_SESSION['registro_exitoso'] = true;
-        header("location: iniciosesion.php");
+    if ($conn->query($sql_insert_persona) === TRUE) {
+        $id_empleado = $conn->insert_id; // Get the last inserted id for the persona
+
+        // Consulta SQL para insertar los datos en la tabla empleado
+        $sql_insert_empleado = "INSERT INTO empleado (Id_Empleado, Num_Doc, Salario, Id_Cargo) 
+                                VALUES ('$id_empleado', '$num_doc', '$salario', '$id_cargo')";
+
+        if ($conn->query($sql_insert_empleado) === TRUE) {
+            // Establecer variable de sesión para indicar registro exitoso
+            $_SESSION['registro_exitoso'] = true;
+            header("Location: registrar.php");
+        } else {
+            echo "Error: " . $sql_insert_empleado . "<br>" . $conn->error;
+        }
     } else {
-        echo "Error: " . $sql_insert . "<br>" . $conn->error;
+        echo "Error: " . $sql_insert_persona . "<br>" . $conn->error;
     }
 }
 
