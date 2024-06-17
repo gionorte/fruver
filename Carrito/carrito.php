85494255c574a3de5525b3ae56b4b28eb3441436
Julian <juliantinoco33@gmail.com>
2024-06-16 19:06:15 -0500

    boton segir comprando

--- Carrito/carrito.php
+++ Carrito/carrito.php
@@ -46,6 +46,7 @@ $conn->close();
     <title>Carrito de Compras</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../js/script.js"></script>
+    <link rel="stylesheet" href="../Assets/css/carrito.css">
     <style>
         table {
             width: 100%;
@@ -64,7 +65,10 @@ $conn->close();
     <h1>Carrito de Compras</h1>
 
     <?php if (empty($productosCarrito)) { ?>
-        <p>El carrito está vacío.</p>
+        <div id="emptyCartAlert" style="display: block;">
+            <p>El carrito está vacío.</p>
+            <button id="continueShoppingButton" onclick="window.location.href='../inter-inicio.php'">Seguir comprando</button>
+        </div>
     <?php } else { ?>
         <table>
             <tr>
@@ -97,7 +101,8 @@ $conn->close();
             <input type="hidden" name="total" value="<?php echo $totalCarrito; ?>">
             <button type="submit">Pagar</button>
         </form>
-        <button onclick="vaciarCarrito()">Vaciar Carrito</button>
+        <button onclick="vaciarCarrito()">Vaciar Carrito</button> 
+  
     <?php } ?>
 
     <script>
